a8063ec31edd4112b3d233a1afc6fe9da915d475
Avighnash <avighnash@gmail.com>
2023-05-15 20:36:55 -0700

    basic structure

---
+++ main.cpp
@@ -0,0 +1,46 @@
+//
+//  main.cpp
+//  BinarySearchTree
+//
+//  Created by Avighnash Kumar on 5/15/23.
+//
+
+#include <iostream>
+
+struct TreeNode {
+    int value;
+    TreeNode * left;
+    TreeNode * right;
+};
+
+class BinarySearchTree {
+public:
+    BinarySearchTree() {
+        this->root = NULL;
+    }
+    
+    void add(int value) {
+        
+    }
+    
+    void del(int value) {
+        
+    }
+    
+    bool doesExist(int value) {
+        return false;
+    }
+    
+    void print() {
+        
+    }
+    
+    
+private:
+    TreeNode * root;
+};
+
+int main() {    
+    
+    return 0;
+}
